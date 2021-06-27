29a6b103ce35c61c8a725b24aea563c6e399cad9
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-06-27 16:51:41 +0200

    Enabled real template rendering for 404 page

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,10 @@ fn index(path: PathBuf) -> Result<Template, Status> {
 async fn not_found<'a>(req: &rocket::Request<'a>) -> Result<Template, &'static str> {
     let metadata = Metadata::from_request(req).await.unwrap();
     if metadata.contains_template("404") {
-        Ok(Template::render("404", ()))
+        match Config::from_file("./data/404.json") {
+            Ok(c) => Ok(Template::render("404", c.context)),
+            Err(_) => Ok(Template::render("404", ())),
+        }
     } else {
         Err("404 not found")
     }
