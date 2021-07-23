6ebab1714237299a44d6faac8cc66032044a398b
andi-makes <26302678+andi-makes@users.noreply.github.com>
2021-07-23 15:47:19 +0200

    markdown helper and refactoring of helpers

---
+++ src/helpers/markdown.rs
@@ -0,0 +1,42 @@
+use super::CustomHelper;
+use rocket_dyn_templates::handlebars::{
+    Context, Handlebars, Helper, HelperResult, Output, RenderContext,
+};
+
+pub struct Helpers;
+
+impl Helpers {
+    fn markdown<'reg: 'rc, 'rc>(
+        h: &Helper,
+        _: &Handlebars,
+        _: &Context,
+        _: &mut RenderContext,
+        out: &mut dyn Output,
+    ) -> HelperResult {
+        use pulldown_cmark::{html, Options, Parser};
+
+        let param = h.param(0).unwrap();
+
+        let path = format!("./data/{}", param.value().as_str().unwrap().trim());
+        let content = std::fs::read_to_string(path).unwrap();
+
+        // Set up options and parser. Strikethroughs are not part of the CommonMark standard
+        // and we therefore must enable it explicitly.
+        let mut options = Options::empty();
+        options.insert(Options::ENABLE_STRIKETHROUGH);
+        let parser = Parser::new_ext(&content, options);
+
+        // Write to String buffer.
+        let mut html_output: String = String::with_capacity(content.len() * 3 / 2);
+        html::push_html(&mut html_output, parser);
+
+        out.write(&html_output)?;
+        Ok(())
+    }
+}
+
+impl CustomHelper for Helpers {
+    fn register(hbs: &mut Handlebars) {
+        hbs.register_helper("markdown", Box::new(Self::markdown));
+    }
+}
---
+++ src/helpers/util.rs
@@ -0,0 +1,29 @@
+use rocket_dyn_templates::handlebars::{
+    Context, Handlebars, Helper, HelperResult, Output, RenderContext,
+};
+
+use super::CustomHelper;
+
+pub fn embed<'reg: 'rc, 'rc>(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> HelperResult {
+    let param = h.param(0).unwrap();
+
+    let path = format!("./data/{}", param.value().as_str().unwrap().trim());
+    let content = std::fs::read_to_string(path).unwrap();
+
+    out.write(&content)?;
+    Ok(())
+}
+
+pub struct Helpers;
+
+impl CustomHelper for Helpers {
+    fn register(hbs: &mut Handlebars) {
+        hbs.register_helper("embed", Box::new(embed));
+    }
+}
